866d21200bae6c7abb22f9003ad7b69b897f6c55
sm003161 <sm003161@naver.com>
2024-05-02 17:26:57 +0900

    Trying different things on Xcode and creating stacking cards as a start.

--- stanford-ios-2024/ContentView.swift
+++ stanford-ios-2024/ContentView.swift
@@ -10,14 +10,45 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+            CardView(isFaceUp: true)
+            CardView()
+            CardView()
+            CardView()
+        }.padding(16)
     }
 }
+
+struct CardView: View {
+    var isFaceUp: Bool = false
+    var body: some View {
+        ZStack {
+            if isFaceUp == false {
+                RoundedRectangle(cornerRadius: 10)
+                    .stroke(style: StrokeStyle(lineWidth: 1))
+                RoundedRectangle(cornerRadius: 10)
+                    .foregroundColor(.white)
+                VStack {
+                    Text("얘들아 안녕").foregroundColor(Color.black)
+                    Text("호호 할머니")
+                }
+            } else {
+                RoundedRectangle(cornerRadius: 10)
+                    .foregroundColor(.blue)
+                VStack {
+                    Text("얘들아 안녕").foregroundColor(Color.white)
+                    Text("호호 할머니").foregroundColor(Color.white)
+                }
+            }
+        }
+        .padding(4)
+    }
+}
+
+
+
+
+
+
 
 #Preview {
     ContentView()
