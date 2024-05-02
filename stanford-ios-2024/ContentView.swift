6e3ce8d1e99efa7804d1df65e7f7d2269c17cd94
sm003161 <sm003161@naver.com>
2024-05-02 16:57:53 +0900

    Initial Commit

---
+++ stanford-ios-2024/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  stanford-ios-2024
+//
+//  Created by Heejae Seo on 5/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ stanford-ios-2024/stanford_ios_2024App.swift
@@ -0,0 +1,17 @@
+//
+//  stanford_ios_2024App.swift
+//  stanford-ios-2024
+//
+//  Created by Heejae Seo on 5/2/24.
+//
+
+import SwiftUI
+
+@main
+struct stanford_ios_2024App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
