0d6ebd69369d98b6123c9d150f9de42621e1f962
sm003161 <sm003161@naver.com>
2024-05-03 18:01:37 +0900

    Replacing the manual content array references with ForEach loop.

--- stanford-ios-2024/ContentView.swift
+++ stanford-ios-2024/ContentView.swift
@@ -8,13 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
-    let contents: [String] = ["1", "2", "3", "4"]
+    let contents: [String] = ["1", "2", "3", "4", "5"]
     var body: some View {
+//      수동 반복
+//        VStack {
+//            ForEach(0..<4, id: \.self) { index in
+//                CardView(content: contents[index])
+//            }
+//        }
+//      Array 안의 데이터 갯수만큼 자동 반복
         VStack {
-            CardView(content: contents[0], isFaceUp: true)
-            CardView(content: contents[1])
-            CardView(content: contents[2])
-            CardView(content: contents[3])
+            ForEach( contents.indices, id: \.self) { index in
+                CardView(content: contents[index])
+            }
         }
         .padding(16)
         .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
@@ -64,7 +70,16 @@ To-do list
  3. ContentView 에서 Array 안에 값을 넣어 이용하기 ( 두 가지 표기법 )
     - Array를 만들어서 컨텐츠 담기
     - Array에서 꺼내서 쓰기
+ 4. ForEach 사용하기 ( id: \.self 포함하기 )
+    - 0~3까지 수동으로 반복하기
+    - Array.indicies를 이용해 자동으로 반복하기
+ 
+ Today's Keyword
+ - View builder
  
  Today's Lesson
  - Argument 순서를 지키기
+ - I can use 3 things in a view builder: the conditionals, the list, the local variables
+ - I cannot use: the for loop
+ - Ranges: s..<d s…d
  */
